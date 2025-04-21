549ef659c0c45ef1b1cdd0213c27968c775b836d
eijiensing <eijitron@gmail.com>
2025-04-21 22:36:29 +0200

    ags

--- modules/home-manager/ags/default.nix
+++ modules/home-manager/ags/default.nix
@@ -1,9 +1,12 @@
-{ inputs, ... }: 
+{ inputs, pkgs, ... }: 
 {
 	imports = [ inputs.ags.homeManagerModules.default ];
 
 	programs.ags = {
 		enable = true;
 		configDir = ./config;
+		extraPackages = with pkgs; [
+			inputs.ags.packages.${pkgs.system}.astal-hyprland
+		];
 	};
 }
