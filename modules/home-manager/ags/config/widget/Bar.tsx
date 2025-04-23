b5d4bfb5503596aea947c8fbd4321aa6d888d833
eijiensing <eijitron@gmail.com>
2025-04-23 07:20:18 +0200

    WOKRING BAR YIIPIE!

--- modules/home-manager/ags/config/widget/Bar.tsx
+++ modules/home-manager/ags/config/widget/Bar.tsx
@@ -55,10 +55,9 @@ function AudioSlider() {
 
 function BatteryLevel() {
     const bat = Battery.get_default()
-
-    return <box className="Battery"
-        visible={bind(bat, "isPresent")}>
-        <icon icon={bind(bat, "batteryIconName")} />
+	return <box className="Battery"
+        visible={bind(bat, "isPresent")}> 
+	<icon icon={bind(bat, "batteryIconName")} />
         <label label={bind(bat, "percentage").as(p =>
             `${Math.floor(p * 100)} %`
         )} />
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -114,6 +114,8 @@
   };
   };
 
+  services.upower.enable = true;
+
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
   services.openssh = {
