eb359d5eb36c4ebf48cf2b71e9061862738b612a
eijiensing <eijitron@gmail.com>
2025-07-13 16:23:36 +0200

    neovim 3

--- modules/home-manager/neovim/lua/luaConfig/plugins/init.lua
+++ modules/home-manager/neovim/lua/luaConfig/plugins/init.lua
@@ -1,4 +1,5 @@
 local colorschemeName = "gruvbox-material"
+vim.g.gruvbox_material_background = "hard"
 vim.cmd.colorscheme(colorschemeName)
 
 local ok, notify = pcall(require, "notify")
@@ -34,22 +35,6 @@ if nixCats('general.extra') then
     },
     keymaps = {
       ["\\"] = { "actions.close", mode = "n" },
-      -- ["g?"] = "actions.show_help",
-      -- ["<CR>"] = "actions.select",
-      -- ["<C-s>"] = "actions.select_vsplit",
-      -- ["<C-h>"] = "actions.select_split",
-      -- ["<C-t>"] = "actions.select_tab",
-      -- ["<C-p>"] = "actions.preview",
-      -- ["<C-c>"] = "actions.close",
-      -- ["<C-l>"] = "actions.refresh",
-      -- ["-"] = "actions.parent",
-      -- ["_"] = "actions.open_cwd",
-      -- ["`"] = "actions.cd",
-      -- ["~"] = "actions.tcd",
-      -- ["gs"] = "actions.change_sort",
-      -- ["gx"] = "actions.open_external",
-      -- ["g."] = "actions.toggle_hidden",
-      -- ["g\\"] = "actions.toggle_trash",
     },
   })
   vim.keymap.set("n", "-", "<cmd>Oil<CR>", { noremap = true, desc = 'Open Parent Directory' })
