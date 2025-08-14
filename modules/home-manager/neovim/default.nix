cdea139064253d4083a2b19f8b13468ff1ee09d7
eijiensing <eijitron@gmail.com>
2025-08-14 10:32:27 +0200

    neovim

--- modules/home-manager/neovim/default.nix
+++ modules/home-manager/neovim/default.nix
@@ -1,5 +1,7 @@
-{ ... }: {
+{ inputs, pkgs, ... }: {
+  home.file.".config/nvim/init.lua".source = ./lua/init.lua;
   programs.neovim = {
     enable = true;
+    package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
   };
 }
