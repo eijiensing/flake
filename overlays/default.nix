a1a526decb156ea25b7779bdbcc0954406a7a604
eijiensing <eijitron@gmail.com>
2025-09-04 12:37:21 +0200

    atlauncher

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,30 +7,7 @@
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
-    atlauncher = prev.atlauncher.overrideAttrs (old: rec {
-      version = "3.4.40.1";
-      pname = "atlauncher";
-
-      mitmCache = final.gradle.fetchDeps {
-        inherit (final);
-
-        pname = "atlauncher";
-        data = ./atlauncher_deps.json;
-      };
-
-      gradleFlags = [
-        "--exclude-task"
-        "createExe"
-        "-Dorg.gradle.java.home=${final.jdk17}"
-      ];
-
-      src = final.fetchFromGitHub {
-        owner = "ATLauncher";
-        repo = "ATLauncher";
-        rev = "v${version}";
-        hash = "sha256-oNWjYSz0lUuhcP/luSM/3u5+nB+g+0YLLyBanoMSmhQ="; # Replace with real hash
-      };
-    });
+		atlauncher = prev.callPackage ../overlays/atlauncher-derivation.nix { };
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
