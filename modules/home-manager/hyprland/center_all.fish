ec7d8c6119fd326fc1c10c4a95715f40f6f486f1
eijiensing <eijitron@gmail.com>
2026-01-01 17:38:48 +0100

    fix center all script

--- modules/home-manager/hyprland/center_all.fish
+++ modules/home-manager/hyprland/center_all.fish
@@ -1,6 +1,8 @@
 #!/usr/bin/env fish
 
 set ws (hyprctl activeworkspace -j | jq -r .id)
+
 for addr in (hyprctl clients -j | jq -r ".[] | select(.workspace.id == $ws) | .address")
-    hyprctl dispatch centerwindow address:$addr
+    hyprctl dispatch focuswindow 0x$addr
+    hyprctl dispatch centerwindow
 end
--- modules/nixos/thunar/default.nix
+++ modules/nixos/thunar/default.nix
@@ -7,7 +7,6 @@
 	services.udisks2.enable = true;
 	environment.systemPackages = with pkgs; [
 		gvfs
-		gvfs-mtp
 		mtpfs
 		libmtp
 	];
