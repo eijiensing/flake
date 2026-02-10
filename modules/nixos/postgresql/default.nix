fe5892dcf389e2b401e8c58b0b8e68bf3456ad6c
eijiensing <eijitron@gmail.com>
2026-02-10 11:01:23 +0100

    disable postgresql on startup

--- modules/nixos/postgresql/default.nix
+++ modules/nixos/postgresql/default.nix
@@ -2,18 +2,19 @@
 {
   services.postgresql = {
     enable = true;
-		package = pkgs.postgresql_16;
-		ensureUsers = [
-			{
-				name = "adlar";
-				ensureDBOwnership = true;
-				ensureClauses.login = true;
-			}
-		];
-		ensureDatabases = [
-			"adlar"
-		];
+    package = pkgs.postgresql_16;
+    ensureUsers = [
+      {
+        name = "adlar";
+        ensureDBOwnership = true;
+        ensureClauses.login = true;
+      }
+    ];
+    ensureDatabases = [
+      "adlar"
+    ];
   };
 
-	systemd.services.postgresql.wantedBy = pkgs.lib.mkForce [ ];
+  systemd.targets.postgresql.wantedBy = pkgs.lib.mkForce [ ];
+  systemd.services.postgresql.wantedBy = pkgs.lib.mkForce [ ];
 }
