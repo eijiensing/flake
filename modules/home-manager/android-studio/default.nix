92626b1bca4edcb1a682058323eb9a340be201f0
eijiensing <eijitron@gmail.com>
2026-02-25 15:27:16 +0100

    android studio

--- modules/home-manager/android-studio/default.nix
+++ modules/home-manager/android-studio/default.nix
@@ -1,5 +1,16 @@
-{ pkgs, ... }: {
-	home.packages = [
-		pkgs.android-studio
-	];
+{ pkgs, ... }:
+{
+  nixpkgs.config = {
+    android_sdk.accept_license = true;
+  };
+
+  home.packages = with pkgs; [
+    android-studio
+    androidenv.androidPkgs.androidsdk
+  ];
+
+  home.sessionVariables = {
+    ANDROID_HOME = "${pkgs.androidenv.androidPkgs.androidsdk}/libexec/android-sdk";
+    ANDROID_SDK_ROOT = "${pkgs.androidenv.androidPkgs.androidsdk}/libexec/android-sdk";
+  };
 }
