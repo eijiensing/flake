cd4ce9bbcef0a58cba68762b34e0e3cf7caff3a6
eijiensing <eijitron@gmail.com>
2026-03-10 12:22:27 +0100

    updated

--- flake.nix
+++ flake.nix
@@ -16,83 +16,86 @@
       url = "github:hyprwm/hyprland-plugins";
       inputs.hyprland.follows = "hyprland";
     };
-		
+
     # Quickshell
     quickshell = {
       url = "github:quickshell-mirror/quickshell?rev=e9bad67619ee9937a1bbecfc6ad3b4231d2ecdc3";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
-    
-		# Neovim
+
+    # Neovim
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-    systems = [
-      "aarch64-linux"
-      "i686-linux"
-      "x86_64-linux"
-    ];
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-  in {
-    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
-    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
-    overlays = import ./overlays {inherit inputs;};
-    nixosModules = import ./modules/nixos;
-    homeManagerModules = import ./modules/home-manager;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
+    let
+      inherit (self) outputs;
+      systems = [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+      overlays = import ./overlays { inherit inputs; };
+      nixosModules = import ./modules/nixos;
+      homeManagerModules = import ./modules/home-manager;
 
-    nixosConfigurations = {
-      desktop = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          ./hosts/desktop/configuration.nix
-          self.nixosModules
-        ];
+      nixosConfigurations = {
+        desktop = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            ./hosts/desktop/configuration.nix
+            self.nixosModules
+          ];
+        };
+        laptop-home = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            ./hosts/laptop-home/configuration.nix
+            self.nixosModules
+          ];
+        };
+        laptop-work = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            ./hosts/laptop-work/configuration.nix
+            self.nixosModules
+          ];
+        };
       };
-      laptop-home = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          ./hosts/laptop-home/configuration.nix
-          self.nixosModules
-        ];
-      };
-      laptop-work = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          ./hosts/laptop-work/configuration.nix
-          self.nixosModules
-        ];
+
+      homeConfigurations = {
+        "eiji@desktop" = home-manager.lib.homeManagerConfiguration {
+          pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [
+            ./hosts/desktop/home.nix
+          ];
+        };
+        "eiji@laptop-home" = home-manager.lib.homeManagerConfiguration {
+          pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [
+            ./hosts/laptop-home/home.nix
+          ];
+        };
+        "eiji@laptop-work" = home-manager.lib.homeManagerConfiguration {
+          pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [
+            ./hosts/laptop-work/home.nix
+          ];
+        };
       };
     };
-
-    homeConfigurations = {
-      "eiji@desktop" = home-manager.lib.homeManagerConfiguration {
-        pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [
-          ./hosts/desktop/home.nix
-        ];
-      };
-      "eiji@laptop-home" = home-manager.lib.homeManagerConfiguration {
-        pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [
-          ./hosts/laptop-home/home.nix
-        ];
-      };
-      "eiji@laptop-work" = home-manager.lib.homeManagerConfiguration {
-        pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [
-          ./hosts/laptop-work/home.nix
-        ];
-      };
-    };
-  };
 }
--- modules/home-manager/bruno/default.nix
+++ modules/home-manager/bruno/default.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
-	home.packages = with pkgs; [
-		bruno
+	home.packages = [
+			pkgs.unstable.bruno
 	];
 }
