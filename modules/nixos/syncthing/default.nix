902f547e2c720b952662c03b21e31c8727c49c5f
eijiensing <eijitron@gmail.com>
2026-04-04 19:33:44 +0200

    syncthing

--- modules/nixos/syncthing/default.nix
+++ modules/nixos/syncthing/default.nix
@@ -4,14 +4,5 @@
     enable = true;
     user = "eiji";
     openDefaultPorts = true;
-    settings = {
-			devices = {
-				vps = { id = "36NEMHN-UT5CQSA-JOTYX3D-TUYZSRL-SJ7RNQF-HF4KDO5-Y35H433-PCMSNQE"; };
-			};
-      folders.default = {
-        path = "/home/eiji/Documents/Sync";
-        devices = [ "vps" ];
-      };
-    };
   };
 }
