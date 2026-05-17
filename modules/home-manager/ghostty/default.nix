68971481e377ade24d823e2493a679eb409c2b49
eijiensing <eijitron@gmail.com>
2026-05-17 20:27:36 +0200

    no ghostty timeout

--- modules/home-manager/ghostty/default.nix
+++ modules/home-manager/ghostty/default.nix
@@ -10,8 +10,7 @@
       font-size = 11;
       window-padding-x = 16;
       window-padding-y = 16;
-			quit-after-last-window-closed = true;
-			quit-after-last-window-closed-delay = "5m";
+			quit-after-last-window-closed = false;
 			resize-overlay = "never";
 			confirm-close-surface = false;
       config-file = "/home/eiji/.config/ghostty/theme.ghostty";
