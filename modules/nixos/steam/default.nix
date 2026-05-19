269d7ed0d41007819ed9cd9449b45a8a5e67b6f7
eijiensing <eijitron@gmail.com>
2026-05-19 18:40:55 +0200

    steam controller

--- modules/nixos/steam/default.nix
+++ modules/nixos/steam/default.nix
@@ -1,10 +1,15 @@
-{ ... }: {
-    programs.steam = {
-        enable = true;
-        gamescopeSession.enable = true;
-    };
-    programs.gamescope = {
-        enable = true;
-    };
-    programs.gamemode.enable = true;
+{ pkgs, ... }:
+{
+  programs.steam = {
+    enable = true;
+    extraPackages = [
+      pkgs.hidapi # this was fixed in steam, by the time you eiji are reading this, its prob fixed
+    ];
+    gamescopeSession.enable = true;
+  };
+  programs.gamescope = {
+    enable = true;
+  };
+  programs.gamemode.enable = true;
+	hardware.steam-hardware.enable = true;
 }
