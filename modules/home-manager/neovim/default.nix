ac50ce72fccafc234f45a0b98f159e627318bd64
eijiensing <eijitron@gmail.com>
2026-06-01 19:41:47 +0200

    update to 26.05

--- modules/home-manager/neovim/default.nix
+++ modules/home-manager/neovim/default.nix
@@ -1,13 +1,16 @@
-{ inputs, pkgs, ... }: {
-  home.file.".config/nvim/init.lua".source = ./nvim/init.lua;
-	home.packages = [
-		pkgs.nixd
-		pkgs.nixfmt
-		pkgs.lua-language-server
-		pkgs.ripgrep
-	];
+{ inputs, pkgs, ... }:
+{
+  home.packages = [
+    pkgs.nixd
+    pkgs.nixfmt
+    pkgs.lua-language-server
+    pkgs.ripgrep
+  ];
   programs.neovim = {
     enable = true;
     package = inputs.neovim-nightly-overlay.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    withPython3 = false;
+    withRuby = false;
+    initLua = builtins.readFile ./nvim/init.lua;
   };
 }
