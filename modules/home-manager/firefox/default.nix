29263ba75a63bbafa4455939437231a6133e1b75
eijiensing <eijitron@gmail.com>
2026-06-09 09:23:33 +0200

    imperative firefox :(

--- modules/home-manager/firefox/default.nix
+++ modules/home-manager/firefox/default.nix
@@ -3,17 +3,17 @@
   programs.firefox = {
     enable = true;
     configPath = "${config.xdg.configHome}/mozilla/firefox";
-    profiles.default = {
-      id = 0;
-      name = "default";
-      isDefault = true;
-      settings = {
-        "browser.startup.page" = 1; # 1 = show homepage, 3 = restore session
-        "browser.sessionstore.resume_from_crash" = false;
-        "browser.sessionstore.max_resumed_crashes" = 0;
-        "browser.sessionstore.enabled" = false;
-      };
-    };
+    # profiles.default = {
+    #   id = 0;
+    #   name = "default";
+    #   isDefault = true;
+    #   settings = {
+    #     "browser.startup.page" = 1; # 1 = show homepage, 3 = restore session
+    #     "browser.sessionstore.resume_from_crash" = false;
+    #     "browser.sessionstore.max_resumed_crashes" = 0;
+    #     "browser.sessionstore.enabled" = false;
+    #   };
+    # };
 
   };
 }
