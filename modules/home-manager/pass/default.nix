e20da8d7314b3931f1ba1009eb46ed2f70f18afe
eijiensing <eijitron@gmail.com>
2026-07-15 15:53:08 +0200

    pass

--- modules/home-manager/pass/default.nix
+++ modules/home-manager/pass/default.nix
@@ -1,5 +1,19 @@
-{ ... }:
+{ pkgs, ... }:
+
 {
+  home.packages = with pkgs; [
+    gnupg
+    pass
+    pinentry-curses
+  ];
+
+  programs.gpg.enable = true;
+
+  services.gpg-agent = {
+    enable = true;
+    pinentry.package = pkgs.pinentry-curses;
+  };
+
   services.pass-secret-service = {
     enable = true;
   };
