142a05ee37313997ae6407963c2223327f2b15c3
eijiensing <eijitron@gmail.com>
2026-09-11 18:03:51 +0200

    wallpapers

--- modules/home-manager/awww/default.nix
+++ modules/home-manager/awww/default.nix
@@ -1,3 +1,8 @@
 { ... }: {
+  home.file."Pictures/Wallpapers/fy_bnb.webp".source = ./wallpapers/fy_bnb.webp;
+  home.file."Pictures/Wallpapers/fixedimmortaltravel.webp".source =
+    ./wallpapers/fixedimmortaltravel.webp;
+  home.file."Pictures/Wallpapers/derrickdoors.webp".source = ./wallpapers/derrickdoors.webp;
+  home.file."Pictures/Wallpapers/kleinsmile.webp".source = ./wallpapers/kleinsmile.webp;
   services.awww.enable = true;
 }
