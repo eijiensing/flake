faf841cbb831c414ac121785d7e9f85877041c7f
eijiensing <eijitron@gmail.com>
2026-09-24 20:16:26 +0200

    vesktop

--- modules/home-manager/discord/default.nix
+++ modules/home-manager/discord/default.nix
@@ -1,17 +1,5 @@
 { pkgs, ... }: {
-    home.packages = with pkgs; [
-    	discord
-    ];
-    xdg.desktopEntries = {
-        discord = {
-            name = "Discord";
-            comment = "Video calls, text messaging, and media.";
-            genericName = "Discord";
-            exec = "discord";
-            terminal = false;
-            categories = [ "AudioVideo" "Network" "Chat"];
-            mimeType = [];
-            icon = "discord";
-        };
-    };
+	programs.discord = {
+		enable = true;
+	};
 }
